81e04699d1f91158a50e4f3455dce565d5024c41
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2024-01-15 14:24:18 -0300

    Update kcdsattak.go

--- kcdsa/kcdsattak/kcdsattak.go
+++ kcdsa/kcdsattak/kcdsattak.go
@@ -33,9 +33,7 @@ var (
 func PPGF(seed []byte, nBits int, domain Domain) []byte {
 	// p.12
 	// from java
-//	i := ((nBits + 7) & 0xFFFFFFF8) / 8
-//	i := ((nBits + 7) & 0xFFFFFFFFFFFFFFF8) / 8
-	i := int64((int64(nBits) + 7)  & 0xFFFFFFFFFFFFFFF8) / 8
+	i := ((nBits + 7) & 0xFFFFFFF8) / 8
 	
 	iBuf := make([]byte, 1)
 
